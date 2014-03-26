03202b194ea68d058912bdc43b349d96262c2479
Jonathan Kemp <jonathan.kemp@cellularsales.com>
2014-03-26 15:54:57 -0400

    Provide alternate search path for assets By default the input files are relative to the treated file. Alternate search path allows one to change that. This would allow for functionality similar to https://github.com/yeoman/grunt-usemin.

--- test/test.js
+++ test/test.js
@@ -1,12 +1,15 @@
+/* jshint node: true */
+/* global describe, it */
+'use strict';
 var expect = require('chai').expect;
 var fs = require('fs');
 var path = require('path');
 var useRef = require('../src/index');
 
-function djoin(p) { 
+function djoin(p) {
   return path.normalize(path.join(__dirname, p));
 }
-function fread(f) { 
+function fread(f) {
   return fs.readFileSync(f, { encoding: 'utf-8'});
 }
 
@@ -16,20 +19,20 @@ describe('html-ref-replace', function() {
   it('should replace reference in css block and return replaced files', function() {
     var result = useRef(fread(djoin('testfiles/01.html')));
     expect(result[0]).to.equal(fread(djoin('testfiles/01-expected.html')));
-    expect(result[1]).to.eql({ css: { '/css/combined.css': [ '/css/one.css', '/css/two.css' ] }});
+    expect(result[1]).to.eql({ css: { '/css/combined.css': { 'assets': [ '/css/one.css', '/css/two.css' ] }}});
   });
 
 
   it('should replace reference in js block and return replaced files', function() {
     var result = useRef(fread(djoin('testfiles/02.html')));
     expect(result[0]).to.equal(fread(djoin('testfiles/02-expected.html')));
-    expect(result[1]).to.eql({ js: { 'scripts/combined.concat.min.js': [ 'scripts/this.js', 'scripts/that.js' ] }});
+    expect(result[1]).to.eql({ js: { 'scripts/combined.concat.min.js': { 'assets': [ 'scripts/this.js', 'scripts/that.js' ] }}});
   });
 
   it('should handle comments and whitespace in blocks', function() {
     var result = useRef(fread(djoin('testfiles/03.html')));
     expect(result[0]).to.equal(fread(djoin('testfiles/03-expected.html')));
-    expect(result[1]).to.eql({ js: { 'scripts/combined.concat.min.js': [ 'scripts/this.js', 'scripts/that.js' ] }});    
+    expect(result[1]).to.eql({ js: { 'scripts/combined.concat.min.js': { 'assets': [ 'scripts/this.js', 'scripts/that.js' ] }}});
   });
 
   it('should handle multiple blocks', function() {
@@ -37,13 +40,23 @@ describe('html-ref-replace', function() {
     expect(result[0]).to.equal(fread(djoin('testfiles/04-expected.html')));
     expect(result[1]).to.eql({
       js: {
-        'scripts/combined.concat.min.js': [ 'scripts/this.js', 'scripts/that.js' ],
-        'scripts/combined2.concat.min.js': [ 'scripts/anotherone.js', 'scripts/yetonemore.js' ]
+        'scripts/combined.concat.min.js': { 'assets': [ 'scripts/this.js', 'scripts/that.js' ] },
+        'scripts/combined2.concat.min.js': { 'assets': [ 'scripts/anotherone.js', 'scripts/yetonemore.js' ] }
       },
       css: {
-        '/css/combined.css': [ '/css/one.css', '/css/two.css' ],
-        '/css/combined2.css': [ '/css/three.css', '/css/four.css' ]
+        '/css/combined.css': { 'assets': [ '/css/one.css', '/css/two.css' ] },
+        '/css/combined2.css': { 'assets': [ '/css/three.css', '/css/four.css' ] }
       }
-    }); 
-  })
+    });
+  });
+
+  it('should return the alternate search path in css block', function() {
+    var result = useRef(fread(djoin('testfiles/05.html')));
+    expect(result[1].css['/css/combined.css'].searchPaths).to.equal('.tmp');
+  });
+
+  it('should return the alternate search path in js block', function() {
+    var result = useRef(fread(djoin('testfiles/06.html')));
+    expect(result[1].js['scripts/combined.concat.min.js'].searchPaths).to.equal('{.tmp,app}');
+  });
 });
